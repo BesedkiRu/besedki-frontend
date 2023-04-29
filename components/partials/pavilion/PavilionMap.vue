9272926056218c5c65a603b1dcf147033e6179ce
lvir <nasibullin1234@mail.ru>
2023-04-29 15:44:59 +0300

    add swagger ts

--- components/partials/pavilion/PavilionMap.vue
+++ components/partials/pavilion/PavilionMap.vue
@@ -1,14 +1,20 @@
 <template>
-  <div></div>
+  <div class="flex flex-col gap-2.5">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { PropType } from 'vue/types/v3-component-props'
+import { PavilionMapEntity } from '~/config/swagger'
 export default Vue.extend({
   name: 'PavilionMap',
   props: {
     map: {
-      type: Object,
+      type: Object as PropType<PavilionMapEntity>,
       required: true,
     },
   },
